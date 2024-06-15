ff397cb6ed00f0e643cdc1b31ac72b5b348c0b2c
ANRomanenko <lifeapricot@gmail.com>
2024-06-16 00:48:19 +0300

    new files

---
+++ automation/glava_2/operators.py
@@ -0,0 +1,22 @@
+eggCount = 42
+eggCount = eggCount <= 42
+print(eggCount)
+
+# Булевые операторы and, or, not
+
+print(True and True)
+print(True and False)
+print()
+print(True or False)
+print(False or True)
+print(False or False)
+print()
+print(not True)
+print(not False)
+print(not not not not True)
+print()
+print((4 < 5) and (5 < 6))
+print((4 < 5) and (9 < 6))
+print((1 == 2) or (2 == 2))
+print(not 2 + 5 == 5)
+print(2 + 2 == 4 and not 2 + 2 == 5 and 2 * 2 == 2 + 2)
