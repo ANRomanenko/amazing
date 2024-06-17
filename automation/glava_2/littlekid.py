e65a28fea4298e05707a66c30c2c69c1212bce52
andredance <lifeapricot@gmail.com>
2024-06-17 22:48:22 +0300

    files

---
+++ automation/glava_2/littlekid.py
@@ -0,0 +1,9 @@
+name = 'Alice'
+age = 11
+
+if name == 'Alice':
+    print('Hi, Alice')
+    if age > 12:
+        print('Внутри блока, код выполнился также')
+else:
+    print('You are neither Alice nor a little kid.')
--- automation/glava_2/vampire2.py
+++ automation/glava_2/vampire2.py
@@ -2,10 +2,21 @@ name = 'Alices'
 age = 3000
 
 if name == 'Alice':
+    print('Hi, Alice\n')
+elif age < 12:
+    print('You are not Alice, kiddo.\n')
+elif age > 100:
+    print('You are not Alice, grannie.\n')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, importal vampire.\n')
+
+
+name = 'Alices'
+age = 11
+
+if name == 'Alice':
     print('Hi, Alice')
 elif age < 12:
     print('You are not Alice, kiddo.')
-elif age > 100:
-    print('You are not Alice, grannie.')
-elif age > 2000:
-    print('Unlike you, Alice is not an undead, importal vampire.')
+else:
+    print('You are neither Alice nor a little kid.')
