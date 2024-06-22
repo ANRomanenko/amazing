f44d9ff6591e753292a3f19b1adf8d54a74ebab1
andredance <lifeapricot@gmail.com>
2024-06-22 13:28:23 +0300

    new scraping

--- scraping/autoria/auto.py
+++ scraping/autoria/auto.py
@@ -1,11 +1,18 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 
-url = ''
+url = "https://auto.ria.com/uk/legkovie/ford/fiesta/state/kiev/?page=1"
 
 headers = {
-
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
 }
 
 response = requests.get(url, headers=headers)
-print(response.status_code)
+
+soup = BeautifulSoup(response.text, "lxml")
+
+data = soup.find("div", class_="content-bar")
+
+title = data.find("div", class_="item ticket-title").get_text(strip=True).replace("Fiesta", "Fiesta ") # Обращаемся к переменной data которая содержит уже нашу карточку товара
+print(title)
