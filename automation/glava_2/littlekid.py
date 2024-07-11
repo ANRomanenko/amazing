37e0f112020c6f66e98de3ee3c010e4ea7f86f2b
ANRomanenko <lifeapricot@gmail.com>
2024-07-11 22:27:03 +0300

    while_new_file

--- automation/glava_2/littlekid.py
+++ automation/glava_2/littlekid.py
@@ -1,5 +1,5 @@
 name = 'Alice'
-age = 11
+age = 14
 
 if name == 'Alice':
     print('Hi, Alice')
@@ -7,3 +7,14 @@ if name == 'Alice':
         print('Внутри блока, код выполнился также')
 else:
     print('You are neither Alice nor a little kid.')
+print()
+
+name_1 = input("Введите любое имя: ")
+age = input("Ведите возраст: ")
+
+if name_1 == name_1:
+    print(f"Hi, {name_1}")
+    if int(age) > 12:
+        print("Эта строка выполниться также!")
+else:
+    print("Все выше действия не верны!")
