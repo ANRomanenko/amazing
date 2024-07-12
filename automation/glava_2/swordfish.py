eb2c59b34916e2bfec983ce0054430427025cbbe
ANRomanenko <lifeapricot@gmail.com>
2024-07-12 16:44:13 +0300

    new_files

--- automation/glava_2/swordfish.py
+++ automation/glava_2/swordfish.py
@@ -1,10 +1,21 @@
-while True:
-    print('Who are you?')
+# while True:
+#     print('Who are you?')
+#     name = input()
+#     if name != 'Joe':
+#         continue
+#     print('Hello, Joe. What is the password? (It is a fish.)')
+#     password = input()
+#     if password == "swordfish":
+#         break
+# print("Acces granted.")
+
+name = ''
+
+while not name:
+    print('Enter your name:')
     name = input()
-    if name != 'Joe':
-        continue
-    print('Hello, Joe. What is the password? (It is a fish.)')
-    password = input()
-    if password == "swordfish":
-        break
-print("Acces granted.")
+print('How many guests will you have?')
+numOfGuests = int(input())
+if numOfGuests:
+    print("Be sure to have enough room for all your guests.")
+print("Done")
